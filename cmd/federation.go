3543f82d0e7853c40282264ac14b18a43577b1bd
Joe Constant <joe@joeconstant.com>
2023-12-24 01:06:19 -0700

    Add close handler

--- cmd/federation.go
+++ cmd/federation.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
+	"os/signal"
 	"sublinks/sublinks-federation/internal/db"
 	"sublinks/sublinks-federation/internal/http"
 	"sublinks/sublinks-federation/internal/log"
 	"sublinks/sublinks-federation/internal/queue"
+	"syscall"
 
 	"github.com/joho/godotenv"
+	"github.com/rabbitmq/amqp091-go"
 )
+
+func SetupCloseHandler(queueService *amqp091.Channel, producer *amqp091.Connection, db *sql.DB) {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Info("Ctrl+C pressed in Terminal")
+		if err := producer.Close(); err != nil {
+			log.Fatal("error during shutdown", err)
+		}
+		if err := queueService.Close(); err != nil {
+			log.Fatal("error during shutdown", err)
+		}
+		if err := db.Close(); err != nil {
+			log.Fatal("error during shutdown", err)
+		}
+		os.Exit(0)
+	}()
+}
 
 func main() {
 	// Load connection string from .env file
@@ -21,19 +44,17 @@ func main() {
 	if err != nil {
 		log.Fatal("failed connecting to db", err)
 	}
-	defer conn.Close()
 	db.RunMigrations(conn)
 
 	mqConnection, err := queue.Connect()
 	if err != nil {
 		log.Fatal("failed connecting to queue service", err)
 	}
-	defer mqConnection.Close()
 	producer, err := queue.CreateProducer(mqConnection, "backend")
 	if err != nil {
 		log.Fatal("failed creating producer", err)
 	}
-	defer producer.Close()
+	SetupCloseHandler(producer, mqConnection, conn)
 	messages, err := queue.CreateConsumer(mqConnection, "federation")
 	if err != nil {
 		log.Fatal("failed creating consumer", err)
