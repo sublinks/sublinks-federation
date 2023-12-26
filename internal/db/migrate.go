02efd9d91b8e48433251afedb1aa174492b74946
Joe Constant <joe@joeconstant.com>
2023-12-26 00:39:18 -0700

    Fix embedding of DB migrations with binary

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -2,13 +2,19 @@ package db
 
 import (
 	"database/sql"
+	"embed"
+	_ "embed"
 	"fmt"
 	"sublinks/sublinks-federation/internal/log"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
+
+//go:embed "migrations"
+var migrations embed.FS
 
 func RunMigrations(db *sql.DB) {
 	log.Debug("Running migrations...")
@@ -16,10 +22,8 @@ func RunMigrations(db *sql.DB) {
 	if err != nil {
 		log.Fatal("Error getting MySQL driver", err)
 	}
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://db/migrations",
-		"mysql", driver,
-	)
+	source, _ := iofs.New(migrations, "migrations")
+	m, err := migrate.NewWithInstance("iofs", source, "mysql", driver)
 	if err != nil {
 		log.Fatal("Error connecting to database", err)
 	}
