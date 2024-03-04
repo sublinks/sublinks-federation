3b112f79dc067446e1ce4fcab65032373313c292
Devan <devandbenz@gmail.com>
2024-03-03 18:45:07 -0600

    working on it

--- internal/queue/consumer.go
+++ internal/queue/consumer.go
@@ -1,16 +1,40 @@
 package queue
 
-import "errors"
+import (
+	"errors"
+)
 
-func (q *RabbitQueue) createConsumer(queueName string) error {
+func (q *RabbitQueue) createConsumer(queueName string, exchangeName string) error {
 	channelRabbitMQ, err := q.Connection.Channel()
 	if err != nil {
 		return err
 	}
-	err = q.createQueue(channelRabbitMQ, queueName)
+
+	err = channelRabbitMQ.ExchangeDeclare(
+		exchangeName,
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
 	if err != nil {
 		return err
 	}
+
+	for _, key := range q.routingKeys {
+		err = q.createQueue(channelRabbitMQ, queueName)
+		if err != nil {
+			return err
+		}
+
+		err = channelRabbitMQ.QueueBind(queueName, key, exchangeName, false, nil)
+		if err != nil {
+			return err
+		}
+	}
+
 	// Subscribing to QueueService1 for getting messages.
 	messages, err := channelRabbitMQ.Consume(
 		queueName, // queue name
@@ -24,24 +48,29 @@ func (q *RabbitQueue) createConsumer(queueName string) error {
 	if err != nil {
 		return err
 	}
+
 	q.consumers[queueName] = messages
+
 	return nil
 }
 
 // TODO: Implement a way to either pass a callback function or return messages/chan
-func (q *RabbitQueue) StartConsumer(queueName string) error {
-	err := q.createConsumer(queueName)
+func (q *RabbitQueue) StartConsumer(queueName string, exchangeName string) error {
+	err := q.createConsumer(queueName, exchangeName)
 	if err != nil {
 		return err
 	}
+
 	messages, ok := q.consumers[queueName]
 	if !ok {
 		return errors.New("consumer not found")
 	}
+
 	go func() {
 		for message := range messages {
 			q.logger.Printf(" > Received message: %s\n", message.Body)
 		}
 	}()
+
 	return nil
 }
